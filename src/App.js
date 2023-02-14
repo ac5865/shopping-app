4c26998cb341342714b608822a9a1ea5e3961563
Anushka Choudhary <66861161+ac5865@users.noreply.github.com>
2023-02-14 16:46:17 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import Header from "./components/Header";
+import CardDetails from "./components/CardDetails";
+import Cards from "./components/Cards";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Cards />} />
+        <Route path="/cart/:id" element={<CardDetails />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
